1ac18c2496676e4894fa6fdc8962e591c2ce127c
meowcat <meowcat@meowcat.site>
2025-12-07 13:07:48 +0000

    fetch type.js

--- js/type.js
+++ js/type.js
@@ -6,7 +6,7 @@ console.log(
   "If you don't know what you are doing, I advise you don't execute any code here. Especially if you have been told to by someone else."
 );
 console.log(
-  "And if you do know what you are doing, you should help me out with some problems! Email me at betacat096@disroot.org!"
+  "And if you do know what you are doing, you should help me out with some problems! Email me at meowcat@meowcat.site!"
 );
 // Create audio elements for different key types
 const normalKeySound = new Audio("https://files.catbox.moe/rsxtlm.mp3");
