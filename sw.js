4d2b0d9a295eb2284f98ca08e29e5b6cad97bad2
meowcat <meowcat@meowcat.site>
2025-12-07 13:16:37 +0000

    update ad configs

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10290231
+    "zoneId": 10290270
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
